e78214d01868e8393a84cc66599e261e5d5812a9
saravana sai <saravanasai002@gmail.com>
2023-08-07 18:42:52 +0530

    Done for 3 sites logic

--- src/contentScript/contentScript.tsx
+++ src/contentScript/contentScript.tsx
@@ -11,31 +11,39 @@ import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import "./contentScript.css";
-import { Button, CardHeader, CardMedia, Divider, Typography } from "@mui/material";
+import {
+  Button,
+  CardHeader,
+  CardMedia,
+  Divider,
+  Typography,
+} from "@mui/material";
 import axios from "axios";
 const App: React.FC<{}> = () => {
   const [value, setValue] = React.useState(0);
 
   const [list, setList] = React.useState([]);
-  const [productQuantitys, setProductQuantity] = React.useState({ });
+  const [productQuantitys, setProductQuantity] = React.useState({});
 
-  const fetchProductInfo = async (productId) => {
+  const fetchProductInfo = async (productId, siteId) => {
     axios.defaults.headers.post["Content-Type"] =
       "application/json;charset=utf-8";
     axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
-    axios.get("http://localhost:3001/" + productId).then(function (response) {
-      if (response.data.data) {
-        setList((prev) => {
-          let newArr = prev;
-          newArr.push(response.data.data);
-          return [
-            ...new Map(
-              newArr.map((item) => [item["product_name"], item])
-            ).values(),
-          ];
-        });
-      }
-    });
+    axios
+      .get("http://localhost:3001/" + productId + "?siteId=" + siteId)
+      .then(function (response) {
+        if (response.data.data) {
+          setList((prev) => {
+            let newArr = prev;
+            newArr.push(response.data.data);
+            return [
+              ...new Map(
+                newArr.map((item) => [item["product_name"], item])
+              ).values(),
+            ];
+          });
+        }
+      });
   };
 
   const addProductQuantity = (itemId) => {
@@ -48,8 +56,8 @@ const App: React.FC<{}> = () => {
   const decreaseProductQuantity = (itemId) => {
     setProductQuantity((prev) => {
       let oldQuantity = prev[itemId] ?? 0;
-       let newQuantity = oldQuantity - 1 ;
-      return { ...prev, [itemId]: newQuantity <0 ? 0 : newQuantity};
+      let newQuantity = oldQuantity - 1;
+      return { ...prev, [itemId]: newQuantity < 0 ? 0 : newQuantity };
     });
   };
 
@@ -57,15 +65,30 @@ const App: React.FC<{}> = () => {
     window.document.addEventListener("mouseover", (e) => {
       //@ts-ignore
       if (e.target.className == "product-record__heading--text") {
-        fetchProductInfo(String(e.target.innerHTML).trim());
+        //@ts-ignore
+        fetchProductInfo(String(e.target.innerHTML).trim(), 1);
+      }
+      //@ts-ignore
+      if (e.target.className == "pt-cv-ctf-value") {
+        //@ts-ignore
+        fetchProductInfo(String(e.target.innerText).trim(), 2);
+      }
+
+      //@ts-ignore
+      if (e.target.className == "yiq Npp") {
+        //@ts-ignore
+        fetchProductInfo(String(e.target.innerText).trim(), 3);
       }
     });
   }, []);
 
   let subTotal = Object.keys(productQuantitys).reduce((acc, key) => {
     let productData = list.filter(({ id }) => (id = key))[0];
-    console.log("🚀 ~ file: contentScript.tsx:66 ~ subTotal ~ productData:", productData?.price)
-    return (acc += Number(productData?.price.substr(1)) * productQuantitys[key]);
+    console.log(
+      "🚀 ~ file: contentScript.tsx:66 ~ subTotal ~ productData:",
+      productData?.price
+    );
+    return (acc += Number(productData?.price) * productQuantitys[key]);
     // return 0;
   }, 0);
 
@@ -101,7 +124,7 @@ const App: React.FC<{}> = () => {
                       />
                       <CardContent>
                         <Typography gutterBottom variant="h5" component="div">
-                          {item.product_name.substr(0,30)}
+                          {item.product_name.substr(0, 30)}
                         </Typography>
                         <Typography variant="body2" color="text.secondary">
                           Price {item.price}
@@ -123,7 +146,6 @@ const App: React.FC<{}> = () => {
                             <span style={{ padding: 3 }}>
                               {productQuantitys[item.id] ?? 0}
                             </span>{" "}
-                            
                             <span
                               style={{ padding: 2 }}
                               onClick={() => addProductQuantity(item.id)}
@@ -146,12 +168,16 @@ const App: React.FC<{}> = () => {
                 );
               })}
 
-           {list.length ? <>
-            <Typography gutterBottom  component="div">
-              SubTotal : {subTotal}
-            </Typography>
-            <Button variant="contained">CheckOut</Button>
-           </> : ''} 
+            {list.length ? (
+              <>
+                <Typography gutterBottom component="div">
+                  SubTotal : {subTotal}
+                </Typography>
+                <Button variant="contained">CheckOut</Button>
+              </>
+            ) : (
+              ""
+            )}
           </CardContent>
           <CardActions>
             <Box sx={{ width: 500 }}>
